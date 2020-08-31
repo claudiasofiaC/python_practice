1de74490485bf53a9e2dd519983646fca3520e77
Claudia Chajon <53544970+claudiasofiaC@users.noreply.github.com>
2020-08-31 16:38:23 -0400

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,19 +1,21 @@
 # number of days per month
 month_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def is_leap(year):
-     """ return True for leap year, False for non-leap year """
 
-     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+def is_leap(year):
+    """ return True for leap year, False for non-leap year """
+
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
 
 def days_in_month(year, month):
-     """ return number of days in that month in that year """
-     if not 1 <= month <= 12:
-         return 'Invalid Month'
+    """ return number of days in that month in that year """
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
 
-     if month == 2 and is_leap(year):
-         return 29
+    if month == 2 and is_leap(year):
+        return 29
 
-     return month_days[month]
+    return month_days[month]
 
 print(days_in_month(2017, 2))
